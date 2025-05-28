6f2b9af6dd7688f7c58c9183aeb274d31f0eb04a
Karthik Reddy <karthikreddy@Karthiks-MacBook-Pro.local>
2025-05-28 16:51:08 +0530

    fix: update base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-const isPreview = process.env.npm_lifecycle_event === "preview"
+const isVercel = !!process.env.VERCEL;
 
 export default defineConfig({
   plugins: [react()],
-  base: isPreview ? "/" : (process.env.VITE_BASE_PATH || "/Karthik_Portfolio"),
+  base: isVercel ? "/" : (process.env.VITE_BASE_PATH || "/Karthik_Portfolio"),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
